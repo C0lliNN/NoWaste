c28683dc987e6878e12b4b77c5d8737c3985c2b1
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-08-21 17:01:05 -0300

    Change Icon, add CSS normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
