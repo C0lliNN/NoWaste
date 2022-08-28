51e593d56adab54bd07e1fe7d7e11fe14bc2941a
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-08-27 21:44:46 -0300

    Make Local Development easier

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import {
   AuthProvider,
+  connectAuthEmulator,
   getAuth,
   GithubAuthProvider,
   GoogleAuthProvider,
@@ -8,18 +9,26 @@ import {
 } from 'firebase/auth';
 import User from '../models/user';
 
+console.log(process.env);
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyA9-kkNVxmYUBNUyPjCOpAqWQH7BowBi5c',
-  authDomain: 'nowaste-7acdd.firebaseapp.com',
-  projectId: 'nowaste-7acdd',
-  storageBucket: 'nowaste-7acdd.appspot.com',
-  messagingSenderId: '732647861949',
-  appId: '1:732647861949:web:4544edb4d11db9fade91d4',
-  measurementId: 'G-9Q97F85R8E'
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
+if (process.env.NODE_ENV !== 'production') {
+  connectAuthEmulator(auth, process.env.REACT_APP_FIREBASE_AUTH_EMULATOR_URL ?? '', {
+    disableWarnings: true
+  });
+}
 
 async function handleFirebaseGoogleLogin(): Promise<User> {
   return await handleLogin(new GoogleAuthProvider());
