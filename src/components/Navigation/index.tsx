96bd662c9cf6dc0624035a747fc29b07eaef560f
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-08-29 19:10:44 -0300

    Update manifest.json

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,5 @@
+import { Container } from './styles';
+
+export default function Navigation(): JSX.Element {
+  return <Container>test</Container>;
+}
---
+++ src/components/Navigation/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  background: #073b4c;
+  height: 72px;
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+`;
